607e1eaea05bf6fd356d7bc707c4bd660521760f
mahiv87 <maherreraiv1987@gmail.com>
2024-05-31 21:19:45 -0600

    styling Footer component

--- src/components/FooterComponent/Footer.jsx
+++ src/components/FooterComponent/Footer.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
 import facebook from '../../assets/images/icon-facebook.svg';
 import instagram from '../../assets/images/icon-instagram.svg';
 import twitter from '../../assets/images/icon-twitter.svg';
 import pinterest from '../../assets/images/icon-pinterest.svg';
 
+import styles from '../FooterComponent/Footer.module.css';
+
 function Footer() {
 	return (
-		<div>
-			<p>sunnyside</p>
+		<div className={styles.container}>
+			<p className={styles.title}>sunnyside</p>
 			<div>
 				<p>About</p>
 				<p>Services</p>
 				<p>Projects</p>
 			</div>
-			<div>
+			<div className={styles.iconContainer}>
 				<img src={facebook} alt="Facebook logo" />
 				<img src={instagram} alt="Instagram logo" />
 				<img src={twitter} alt="Twitter logo" />

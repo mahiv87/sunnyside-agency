75b18f6e48a192e2e90e369a25adfff39bb671db
mahiv87 <maherreraiv1987@gmail.com>
2024-06-02 19:17:25 -0600

    Card component init

---
+++ src/components/CardComponent/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Card(props) {
+	return (
+		<div>
+			<p>Transform your brand</p>
+			<p>
+				We are a full-service creative agency specializing in helping brands
+				grow fast. Engage your clients through compelling visuals that do most
+				of the marketing for you.
+			</p>
+			<p>LEARN MORE</p>
+		</div>
+	);
+}
+
+Card.propTypes = {};
+
+export default Card;

15f2132998dc8e3a72aef5f1431a5a85359d37cb
Marcus mbp <maherreraiv1987@gmail.com>
2024-06-04 21:47:01 -0600

    imported header image

--- src/components/HeroComponent/Hero.jsx
+++ src/components/HeroComponent/Hero.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import Header from '../../assets/images/mobile/image-header.jpg';
+
 function Hero() {
 	return (
 		<div>
+			<img src={Header} alt="" />
 			<p>WE ARE CREATIVES</p>
 		</div>
 	);

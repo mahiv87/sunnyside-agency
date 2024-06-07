e5ab23ed14e7762d9a7bc106f5d7d41285478624
Marcus mbp <maherreraiv1987@gmail.com>
2024-06-06 20:01:09 -0600

    added nav logos

--- src/components/HeroComponent/Hero.jsx
+++ src/components/HeroComponent/Hero.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import logo from '../../assets/images/logo.svg';
+import hamburger from '../../assets/images/icon-hamburger.svg';
 import ArrowImage from '../../assets/images/icon-arrow-down.svg';
 import styles from '../HeroComponent/Hero.module.css';
 
 function Hero() {
 	return (
-		<div className={styles.container}>
-			<p className={styles.title}>WE ARE CREATIVES</p>
-			<img src={ArrowImage} alt="" />
+		<div>
+			<div className={styles.container}>
+				<div className={styles.nav}>
+					<img src={logo} alt="sunnyside logo" />
+					<img src={hamburger} alt="hamburger icon for menu" />
+				</div>
+				<p className={styles.title}>WE ARE CREATIVES</p>
+				<img src={ArrowImage} alt="" />
+			</div>
 		</div>
 	);
 }

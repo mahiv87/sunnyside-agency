01f6ed26b86b3ff2826d53ffb80072a6a8113834
mahiv87 <maherreraiv1987@gmail.com>
2024-06-11 20:29:53 -0600

    init hamburger menu

--- src/components/HeroComponent/Hero.jsx
+++ src/components/HeroComponent/Hero.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
+
 import logo from '../../assets/images/logo.svg';
 import hamburger from '../../assets/images/icon-hamburger.svg';
 import ArrowImage from '../../assets/images/icon-arrow-down.svg';
 import styles from '../HeroComponent/Hero.module.css';
 
 function Hero() {
+	const [menuOpen, setMenuOpen] = useState(false);
+
 	return (
 		<div>
 			<div className={styles.container}>
 				<div className={styles.nav}>
 					<img src={logo} alt="sunnyside logo" />
-					<img src={hamburger} alt="hamburger icon for menu" />
+					<img src={hamburger} alt="hamburger icon for menu" id="menu" />
 				</div>
 				<p className={styles.title}>WE ARE CREATIVES</p>
 				<img src={ArrowImage} alt="" />

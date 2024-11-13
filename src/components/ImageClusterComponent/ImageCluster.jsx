1d4eb352154a3f3c707a8ab507b7d89b0872d94d
Marcus mbp <maherreraiv1987@gmail.com>
2024-11-13 09:38:03 -0700

    image gallery section done

--- src/components/ImageClusterComponent/ImageCluster.jsx
+++ src/components/ImageClusterComponent/ImageCluster.jsx
@@ -1,10 +1,10 @@
 import styles from '../ImageClusterComponent/ImageCluster.module.css';
 import Image from './ImageComponent/Image';
 
-import MilkBottles from '../../assets/images/mobile/image-gallery-milkbottles.jpg';
-import Cone from '../../assets/images/mobile/image-gallery-cone.jpg';
-import SugarCubes from '../../assets/images/mobile/image-gallery-sugar-cubes.jpg';
-import Orange from '../../assets/images/mobile/image-gallery-orange.jpg';
+import MilkBottles from '../../assets/images/desktop/image-gallery-milkbottles.jpg';
+import Cone from '../../assets/images/desktop/image-gallery-cone.jpg';
+import SugarCubes from '../../assets/images/desktop/image-gallery-sugarcubes.jpg';
+import Orange from '../../assets/images/desktop/image-gallery-orange.jpg';
 
 function ImageCluster() {
 	return (

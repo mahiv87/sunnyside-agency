ca8061e10c6502a9f5ac27a15cc5ff7a7e709596
Marcus mbp <maherreraiv1987@gmail.com>
2024-11-13 09:23:53 -0700

    testimonial section done

--- src/components/TestimonialsComponent/Testimonials.jsx
+++ src/components/TestimonialsComponent/Testimonials.jsx
@@ -9,30 +9,32 @@ function Testimonials() {
 	return (
 		<div className={styles.container}>
 			<p className={styles.title}>CLIENT TESTIMONIALS</p>
-			<Testimonial
-				avatar={EmilyR}
-				text={
-					'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
-				}
-				author={'Emily R.'}
-				title={'Marketing Director'}
-			/>
-			<Testimonial
-				avatar={ThomasS}
-				text={
-					'Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
-				}
-				author={'Thomas S.'}
-				title={'Chief Operating Officer'}
-			/>
-			<Testimonial
-				avatar={JennieF}
-				text={
-					'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
-				}
-				author={'Jennie F.'}
-				title={'Business Owner'}
-			/>
+			<div className={styles.testimonialContainer}>
+				<Testimonial
+					avatar={EmilyR}
+					text={
+						'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.'
+					}
+					author={'Emily R.'}
+					title={'Marketing Director'}
+				/>
+				<Testimonial
+					avatar={ThomasS}
+					text={
+						'Sunnyside’s enthusiasm coupled with their keen interest in our brand’s success made it a satisfying and enjoyable experience.'
+					}
+					author={'Thomas S.'}
+					title={'Chief Operating Officer'}
+				/>
+				<Testimonial
+					avatar={JennieF}
+					text={
+						'Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
+					}
+					author={'Jennie F.'}
+					title={'Business Owner'}
+				/>
+			</div>
 		</div>
 	);
 }
